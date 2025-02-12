50811b9cf0bcaee1fdff8ab74f5d68c299bb36bf
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2025-02-12 00:02:53 -0600

    [WIP] FeData4 write

--- src/App/Vivianne/Controls/DoubleCollectionEditor.cs
+++ src/App/Vivianne/Controls/DoubleCollectionEditor.cs
@@ -1,10 +1,6 @@
-﻿using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.Windows;
-using System.Windows.Controls;
-using TheXDS.MCART.Helpers;
+﻿using System.Windows.Controls;
+using TheXDS.Vivianne.Controls.Base;
 using Wpf.Ui.Controls;
-using static TheXDS.MCART.Helpers.DependencyObjectHelpers;
 
 namespace TheXDS.Vivianne.Controls;
 
@@ -13,56 +9,4 @@ namespace TheXDS.Vivianne.Controls;
 /// <see cref="NumberBox"/> controls that allow the user to directly edit a
 /// collection of <see cref="double"/> values.
 /// </summary>
-public class DoubleCollectionEditor : ItemsControl
-{
-    /// <summary>
-    /// Identifies the <see cref="ItemsSource"/> dependency property.
-    /// </summary>
-    public static readonly new DependencyProperty ItemsSourceProperty;
-
-    /// <summary>
-    /// Gets or sets the list to be displayed and updated through this control.
-    /// </summary>
-    public new IList<double> ItemsSource
-    {
-        get => (IList<double>)GetValue(ItemsSourceProperty);
-        set => SetValue(ItemsSourceProperty, value);
-    }
-    static DoubleCollectionEditor()
-    {
-        ItemsSourceProperty = NewDp<IList<double>, DoubleCollectionEditor>(nameof(ItemsSource), [], OnItemsSourceChanged);
-    }
-
-    private static void OnItemsSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-    {
-        var control = (DoubleCollectionEditor)d;
-
-        void OnCollectionChanged(object? _, NotifyCollectionChangedEventArgs e)
-        {
-            var nud = control.Items.Cast<NumberBox>().First(p => (int)p.Tag == e.NewStartingIndex);
-            var value = (double?)e.NewItems?[0];
-            if (nud.Value != value) nud.Value = value;
-        }
-
-        if (e.OldValue is ObservableCollection<double> oldC) oldC.CollectionChanged -= OnCollectionChanged;
-        if (e.NewValue is ObservableCollection<double> c) c.CollectionChanged += OnCollectionChanged;
-        
-        control.Items.Clear();
-        foreach ((var index, var value) in (e.NewValue as IEnumerable<double> ?? []).WithIndex())
-        {
-            var nud = new NumberBox
-            {
-                Value = value,
-                Tag = index
-            };
-            nud.ValueChanged += (sender, e) =>
-            {
-                if (control.ItemsSource[(int)nud.Tag] != nud.Value)
-                {
-                    control.ItemsSource[(int)nud.Tag] = nud.Value ?? 0.0;
-                }
-            };
-            control.Items.Add(nud);
-        }
-    }
-}
+public class DoubleCollectionEditor : CollectionEditorBase<double>;
--- src/Lib/VivLib/Serializers/FeData4Serializer.cs
+++ src/Lib/VivLib/Serializers/FeData4Serializer.cs
@@ -26,6 +26,18 @@ public class FeData4Serializer : ISerializer<FeData4>
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     private struct FeDataHeader
     {
+        private static FeDataHeader Empty = new()
+        {
+            Magic = 0x04,
+            Padding_0x1 = new byte[0x111],
+            CarId = new string(' ', 4),
+            Padding_0x116 = new byte[0x208],
+            Padding_0x320 = new byte[0x5a],
+            Unk_0x386 = 0xb5,
+            Unk_0x387 = 0x01,
+            Unk_0x3ae = new byte[0x10]
+        };
+
         public byte Magic; // = 0x04
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 0x111)]
         public byte[] Padding_0x1;
@@ -38,13 +50,18 @@ public class FeData4Serializer : ISerializer<FeData4>
         public byte[] Padding_0x320;
         public byte PoliceFlag_IsBonus;
         public byte Upgradable_Convertible;
-        public ushort Unk_0x37c;
-        public ushort Unk_0x37e;
-        public ushort Unk_0x380;
+        public byte Unk_0x37c;
+        public byte Unk_0x37d;
+        public byte Unk_0x37e;
+        public byte Unk_0x37f;
+        public byte Unk_0x380;
+        public byte Unk_0x381;
         public Nfs4CarClass CarClass;
         public byte Unk_0x383;
-        public ushort Unk_0x384;
-        public ushort Unk_0x386;
+        public byte Unk_0x384;
+        public byte Unk_0x385;
+        public byte Unk_0x386; // 0xb5 or 0x80
+        public byte Unk_0x387; // 0x01 or 0x00
         public byte CompareCount; // x2, this seems to indicate the number of ushort values that form the compare tables (actual table values are 1 byte each), always = 0x0a
         public CompareTableItem Acceleration;
         public CompareTableItem TopSpeed;
