affe865a1a539a97666c9060750216449b6b5a20
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2025-02-27 09:55:29 -0600

    UI Tweaks

--- src/App/Vivianne/Controls/DraggableLineGraph.cs
+++ src/App/Vivianne/Controls/DraggableLineGraph.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.ObjectModel;
-using System.Collections.Specialized;
+﻿using System.Collections.Specialized;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
