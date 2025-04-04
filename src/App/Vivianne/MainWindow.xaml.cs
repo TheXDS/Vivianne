92a1bd4b9d50146eba9ad905280dd8a7f305daa6
César Morgan <xds_xps_ivx@hotmail.com>
2025-04-04 01:43:29 -0600

    More tweaks to styling

--- src/App/Vivianne/MainWindow.xaml.cs
+++ src/App/Vivianne/MainWindow.xaml.cs
@@ -12,26 +12,15 @@ public partial class MainWindow
     /// </summary>
     public MainWindow()
     {
-        //Wpf.Ui.Appearance.SystemThemeWatcher.Watch(this);
+        Wpf.Ui.Appearance.SystemThemeWatcher.Watch(this);
         InitializeComponent();
         if (OperatingSystem.IsWindowsVersionAtLeast(10, build: 26100))
         {
             WindowBackdropType = Wpf.Ui.Controls.WindowBackdropType.Mica;
         }
-        else
+        else if (OperatingSystem.IsWindowsVersionAtLeast(10, build: 18362))
         {
-            Background = new SolidColorBrush(Color.FromArgb(0xff, 0x40, 0x40, 0x40));
-            if (OperatingSystem.IsWindowsVersionAtLeast(10, build: 19041))
-            {
-                WindowBackdropType = Wpf.Ui.Controls.WindowBackdropType.Auto;
-            }
+            WindowBackdropType = Wpf.Ui.Controls.WindowBackdropType.Acrylic;
         }
-    }
-
-    private int currentTheme;
-
-    private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
-    {
-        Wpf.Ui.Appearance.ApplicationThemeManager.Apply((Wpf.Ui.Appearance.ApplicationTheme)((currentTheme = 1 - currentTheme) + 1));
     }
 }
