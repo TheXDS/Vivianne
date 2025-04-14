8eefb3f1493287023b8454e14f7b2b70daeed85e
César Morgan <xds_xps_ivx@hotmail.com>
2025-04-13 21:40:44 -0600

    Add carp-to-fedata sync on nfs4

--- src/App/Vivianne.Common/ViewModels/Carp/Nfs4/CarpEditorViewModel.cs
+++ src/App/Vivianne.Common/ViewModels/Carp/Nfs4/CarpEditorViewModel.cs
@@ -1,9 +1,22 @@
-﻿using TheXDS.Vivianne.Models.Carp.Nfs4;
+﻿using System.Collections.Generic;
+using TheXDS.Vivianne.Models.Carp.Nfs4;
 using TheXDS.Vivianne.Models.Fe.Nfs4;
+using TheXDS.Vivianne.Properties;
+using TheXDS.Vivianne.Tools.Fe;
 
 namespace TheXDS.Vivianne.ViewModels.Carp.Nfs4;
 
 /// <summary>
 /// Implements a ViewModel for NFS4 Carp files.
 /// </summary>
-public class CarpEditorViewModel : CarpEditorViewModel<CarpEditorState, CarPerf, CarClass>;
+public class CarpEditorViewModel : CarpEditorViewModel<CarpEditorState, CarPerf, CarClass>
+{     /// <inheritdoc/>
+    protected override bool BeforeSave()
+    {
+        if (Settings.Current.Carp_SyncChanges)
+        {
+            FeData4SyncTool.Sync(State.File, BackingStore?.Store.AsDictionary() ?? new Dictionary<string, byte[]>());
+        }
+        return base.BeforeSave();
+    }
+}
