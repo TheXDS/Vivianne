29ca123f8bd01636b7215221132dc51082050365
César Morgan <xds_xps_ivx@hotmail.com>
2025-04-17 21:16:44 -0600

    UI Fixes

---
+++ src/App/Vivianne/Controls/HexByteNumberFormatter.cs
@@ -0,0 +1,28 @@
+﻿namespace TheXDS.Vivianne.Controls;
+
+/// <summary>
+/// Formats <see cref="byte"/> numbers as hex values.
+/// </summary>
+public class HexByteNumberFormatter : HexNumberFormatter<byte>
+{
+    /// <inheritdoc/>
+    protected override byte From(double? value) => (byte)(value ?? 0);
+
+    /// <inheritdoc/>
+    protected override byte From(int? value) => (byte)(value ?? 0);
+
+    /// <inheritdoc/>
+    protected override byte From(uint? value) => (byte)(value ?? 0);
+
+    /// <inheritdoc/>
+    protected override byte[] GetBytes(byte value) => [value];
+
+    /// <inheritdoc/>
+    protected override double? ToDouble(byte? value) => value;
+
+    /// <inheritdoc/>
+    protected override int? ToInt(byte? value) => value;
+
+    /// <inheritdoc/>
+    protected override uint? ToUInt(byte? value) => value;
+}
