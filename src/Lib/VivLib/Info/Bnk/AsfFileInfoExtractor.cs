66633b67852e67a7092e91685d61cf0a0cb4f246
César Morgan <xds_xps_ivx@hotmail.com>
2025-07-17 01:13:51 -0600

    Additional ASF data

--- src/Lib/VivLib/Info/Bnk/AsfFileInfoExtractor.cs
+++ src/Lib/VivLib/Info/Bnk/AsfFileInfoExtractor.cs
@@ -1,4 +1,5 @@
-﻿using TheXDS.MCART.Types.Extensions;
+﻿using TheXDS.MCART.Math;
+using TheXDS.MCART.Types.Extensions;
 using TheXDS.Vivianne.Extensions;
 using TheXDS.Vivianne.Models.Audio.Base;
 using TheXDS.Vivianne.Models.Audio.Mus;
@@ -33,9 +34,11 @@ public class AsfFileInfoExtractor(bool humanSize) : IEntityInfoExtractor<AsfFile
                 string.Format(St.BnkNfo_Format, value.BytesPerSample * 8, Mappings.AudioCodecDescriptions.GetValueOrDefault(value.Compression, "Unknown")),
                 string.Format(St.BnkNfo_SampleRate, value.SampleRate),
                 string.Format(St.BnkNfo_Size, totalBytes.GetSize(humanSize)),
+                string.Format("Total audio blocks: {0}", value.AudioBlocks.Count),
+                string.Format("Average chunk length: {0} samples", value.AudioBlocks.Select(p => (double)p.Length).Mode().First() / value.BytesPerSample),
+                value.LoopOffset.HasValue ? string.Format("SCLl Loop offset: {0} ({1}) ", value.LoopOffset.Value, FromSample(value.LoopOffset.Value, value)) : null,
                 string.Format("PT Loop start: {0} ({1})", value.LoopStart * value.Channels, FromSample(value.LoopStart * value.Channels, value)),
                 string.Format("PT Loop end: {0} ({1})", value.LoopEnd * value.Channels, FromSample(value.LoopEnd * value.Channels, value)),
-                value.LoopOffset.HasValue ? string.Format("SCLl Loop offset: {0} ({1}) ", value.LoopOffset.Value, FromSample(value.LoopOffset.Value, value)) : null,
             }.NotNull(),
             .. value.Properties.Select(p => $"PTHeader {p.Key:X2}: {p.Value.Value} (0x{p.Value.Value:X8})")
         ];
